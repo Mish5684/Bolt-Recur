3ce076a35791888a103f5d2fd5a6af78da405e3b
Mihir Shah <mish@Mihirs-iMac.local>
2025-12-11 13:59:17 +0530

    fixing builds

--- shared/api/supabase.ts
+++ shared/api/supabase.ts
@@ -1,7 +1,6 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Platform } from 'react-native';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || '';
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '';
@@ -12,12 +11,10 @@ if (!supabaseUrl || !supabaseAnonKey) {
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    ...(Platform.OS !== 'web' ? { storage: AsyncStorage } : {}),
-    // 🟢 ADD THIS LINE TO DISABLE WEBAUTHN INIT AND USE MOBILE-STANDARD FLOW
-    flowType: 'pkce',
-    webAuthn: false,
+    storage: AsyncStorage,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
+    flowType: 'pkce',
   },
 });
